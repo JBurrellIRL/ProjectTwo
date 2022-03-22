404668b2ade2c562064644de8c9d31485dc8dcb5
JBurrellIRL <jburrellirl@gmail.com>
2022-03-22 16:08:31 +0000

    Add function to change winning score based on game selected, and alert for choosing a game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,28 @@
-// Global variables for buttons and select area in scoreboard
-
-// Buttons
 const p1Button = document.getElementById('p1+1');
-const p2Button = document.getElementById('p1+2');
+const p2Button = document.getElementById('p2+1');
 const resetButton = document.getElementById('resetgame');
 
-// Score Display
 const p1ScoreDisplay = document.getElementById('p1scoredisplay');
 const p2ScoreDisplay = document.getElementById('p2scoredisplay');
 
-// Game selection
+const p1TotalDisplay = document.getElementsByClassName('score1')
+const p2TotalDisplay = document.getElementsByClassName('score2')
+
 const chooseGame = document.getElementById('choosegame');
 
-// Variables for player totals
 let p1Total = 0;
 let p2Total = 0;
 
-// Variables for logical operation of game totals
+
 let winningScore = 0;
 let gameOver = false;
 
-// Variables for overall game score, games play to 3
 let totalScoreP1 = 0;
 let totalScoreP2 = 0;
 
-/* Player One button - increase score, and then stop when chosen game
-is over. Also adding green/red border for winning/losing player */
+
+
+// Player One button - increase score, and also stop when winning score is reached (game over)
 
 p1Button.addEventListener('click', function () {
     if (!gameOver) {
@@ -34,13 +31,14 @@ p1Button.addEventListener('click', function () {
             gameOver = true;
             p1ScoreDisplay.style.border = "5px solid green";
             p2ScoreDisplay.style.border = "5px solid red";
+            incrementScoreP1();
+            // oldScore +=1;
         }
         p1ScoreDisplay.innerHTML = p1Total;
     }
 })
 
-/* Player Two button - increase score, and then stop when chosen game
-is over. Also adding green/red border for winning/losing player */
+// Player Two button - increase score, and also stop when winning score is reached (game over)
 
 p2Button.addEventListener('click', function () {
     if (!gameOver) {
@@ -49,13 +47,13 @@ p2Button.addEventListener('click', function () {
             gameOver = true;
             p1ScoreDisplay.style.border = "5px solid red";
             p2ScoreDisplay.style.border = "5px solid green";
+            incrementScoreP2();
         }
-        p1ScoreDisplay.innerHTML = p1Total;
+        p2ScoreDisplay.innerHTML = p2Total;
     }
 })
 
-/* Reset button resets both scores and updates displayed scores 
-to zero for both players */
+// Reset button, to reset scores and update score display to 0 
 
 resetButton.addEventListener('click', function () {
     p1Total = 0;
@@ -67,3 +65,63 @@ resetButton.addEventListener('click', function () {
     p1ScoreDisplay.style.border = 'initial';
     p2ScoreDisplay.style.border = 'initial';
 })
+
+// Alert to tell you to select game after clicking on either Player One or Player Two buttons 
+
+p1Button.addEventListener('click', function () {
+    if (winningScore === 0) {
+        alert("Please select your game!");
+        p1Total = 0;
+        p1ScoreDisplay.innerHTML = 0;
+    }
+})
+
+p2Button.addEventListener('click', function () {
+    if (winningScore === 0) {
+        alert("Please select your game!");
+        p2Total = 0;
+        p2ScoreDisplay.innerHTML = 0;
+    }
+})
+
+// Code for different winning scores based on game selected 
+
+chooseGame.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    p1Total = 0;
+    p2Total = 0;
+    totalScoreP1 = 0;
+    totalScoreP2 = 0;
+    p1ScoreDisplay.innerHTML = 0;
+    p2ScoreDisplay.innerHTML = 0;
+    document.getElementById("scorep1").innerText = 0;
+    document.getElementById("scorep2").innerText = 0;
+    // set gameOver variable back to false, to begin game again 
+    gameOver = false;
+    p1ScoreDisplay.style.border = 'initial';
+    p2ScoreDisplay.style.border = 'initial';
+})
+
+function incrementScoreP1() {
+    document.getElementById("scorep1").innerText = totalScoreP1 += 1;
+    if (totalScoreP1 === 3) {
+        totalScoreP1 = 0;
+        totalScoreP2 = 0;
+        document.getElementById("scorep1").innerText = 0;
+        document.getElementById("scorep2").innerText = 0;
+        alert("Player One Wins");
+
+    }
+}
+
+function incrementScoreP2() {
+    document.getElementById("scorep2").innerText = totalScoreP2 += 1;
+    if (totalScoreP2 === 3) {
+        totalScoreP1 = 0;
+        totalScoreP2 = 0;
+        document.getElementById("scorep1").innerText = 0;
+        document.getElementById("scorep2").innerText = 0;
+        alert("Player Two Wins");
+
+    }
+}
