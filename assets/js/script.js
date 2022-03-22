906cb748b5a76214f535465ec8f195780664fb2e
JBurrellIRL <jburrellirl@gmail.com>
2022-03-22 15:29:46 +0000

    Add event listeners for player buttons and reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,47 @@ let gameOver = false;
 // Variables for overall game score, games play to 3
 let totalScoreP1 = 0;
 let totalScoreP2 = 0;
+
+/* Player One button - increase score, and then stop when chosen game
+is over. Also adding green/red border for winning/losing player */
+
+p1Button.addEventListener('click', function () {
+    if (!gameOver) {
+        p1Total += 1;
+        if (p1Total === winningScore) {
+            gameOver = true;
+            p1ScoreDisplay.style.border = "5px solid green";
+            p2ScoreDisplay.style.border = "5px solid red";
+        }
+        p1ScoreDisplay.innerHTML = p1Total;
+    }
+})
+
+/* Player Two button - increase score, and then stop when chosen game
+is over. Also adding green/red border for winning/losing player */
+
+p2Button.addEventListener('click', function () {
+    if (!gameOver) {
+        p2Total += 1;
+        if (p2Total === winningScore) {
+            gameOver = true;
+            p1ScoreDisplay.style.border = "5px solid red";
+            p2ScoreDisplay.style.border = "5px solid green";
+        }
+        p1ScoreDisplay.innerHTML = p1Total;
+    }
+})
+
+/* Reset button resets both scores and updates displayed scores 
+to zero for both players */
+
+resetButton.addEventListener('click', function () {
+    p1Total = 0;
+    p2Total = 0;
+    p1ScoreDisplay.innerHTML = 0;
+    p2ScoreDisplay.innerHTML = 0;
+    // set gameOver variable back to false, to begin game again 
+    gameOver = false;
+    p1ScoreDisplay.style.border = 'initial';
+    p2ScoreDisplay.style.border = 'initial';
+})
